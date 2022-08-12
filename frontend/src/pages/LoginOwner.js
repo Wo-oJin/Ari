ede14291737abd20594db8420febe09396f953c0
salmonco <xkqjsslsek80@naver.com>
2022-08-13 00:14:35 +0900

    Style: styled-components를 css로 변경

--- frontend/src/pages/LoginOwner.js
+++ frontend/src/pages/LoginOwner.js
@@ -1,21 +1,14 @@
 import { React } from 'react';
 import { Link } from "react-router-dom";
 import MainButton from '../components/common/Mainbutton';
-import styled from 'styled-components';
 import Header from '../components/Header';
-
-const ButtonContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-`;
 
 const LoginStore = () => {
     return (
         <>
             <Header text="로그인/회원가입" link="/loginRegister"></Header>
             <div className="logoContainer"LogoContainer></div>
-            <ButtonContainer>
+            <div className="buttonContainer">
                 <Link to="/login">
                     <MainButton
                         radius="15px"
@@ -32,7 +25,7 @@ const LoginStore = () => {
                         text="이메일로 회원가입"
                     />
                 </Link>
-            </ButtonContainer>
+            </div>
         </>
     )
 }
