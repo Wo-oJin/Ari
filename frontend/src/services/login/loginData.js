b5ee5090884b62c735fc2f87ccd629b3000e89d7
salmonco <xkqjsslsek80@naver.com>
2022-08-14 23:39:23 +0900

    Feat: 로그인

--- frontend/src/services/login/loginData.js
+++ frontend/src/services/login/loginData.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const POST_BASE_URL = "http://.../login";
+const POST_BASE_URL = "/auth/login";
 
 export const loginData = async (inputData) => {
     try {
@@ -11,6 +11,14 @@ export const loginData = async (inputData) => {
                 password: inputData.password,
             }
         );
+        // console.log("response>>"+JSON.stringify(response));
+        const { accessToken } = response.data.data;
+        // API 요청하는 콜마다 헤더에 accessToken 담아 보내도록 설정
+        // console.log("accessToken>>"+accessToken);
+		axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
+
+		// accessToken을 localStorage, cookie 등에 저장하지 않는다!
+
         return response.data;
     } catch (e) {
         console.log(e);
