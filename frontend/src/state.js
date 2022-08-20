2abe028d6e93ee96340070cbff0d4a863a348516
salmonco <xkqjsslsek80@naver.com>
2022-08-20 22:27:18 +0900

    Add: logout

--- frontend/src/state.js
+++ frontend/src/state.js
@@ -2,17 +2,16 @@ import { atom } from "recoil";
 import { recoilPersist } from "recoil-persist";
 const { persistAtom } = recoilPersist();
 
-// recoil로 관리할 변수들
-// email
-export const emailState = atom({
-  key: "emailState",
-  default: "",
+// auth
+export const authState = atom({
+  key: "authState",
+  default: 0,
   effects_UNSTABLE: [persistAtom],
 });
 
 // nickname
-export const nicknameState = atom({
-  key: "nicknameState",
+export const nameState = atom({
+  key: "nameState",
   default: "",
   effects_UNSTABLE: [persistAtom],
 });
