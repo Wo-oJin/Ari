df9d15b6a6bf38a22ca9f9886fe9341a911be5cf
salmonco <xkqjsslsek80@naver.com>
2022-08-27 21:04:48 +0900

    Add: Oauth 로그인 - redirect 아직 안 구현됨

--- frontend/src/pages/LoginUser.js
+++ frontend/src/pages/LoginUser.js
@@ -1,20 +1,50 @@
-import { React } from 'react';
+import { React, useEffect} from 'react';
 import { Link } from "react-router-dom";
 import MainButton from '../components/common/Mainbutton';
 import "./LoginUser.css";
 import Header from '../components/Header';
+import axios from 'axios';
 
 const LoginUser = () => {
+    let kakao;
+    let naver;
+
+    // Oauth 로그인
+    const getOauthURL = async () => {
+        try {
+            await axios
+                .get("/auth/login")
+                .then(res => {
+                    // console.log("res>>"+JSON.stringify(res));
+                    kakao = res.data.kakao;
+                    naver = res.data.naver;
+                })
+        } catch (e) {
+            console.log(e);
+        }
+    }
+    useEffect(() => {
+        getOauthURL();
+    }, []);
+
+    const onKakao = () => {
+        window.location.href = kakao;
+    }
+
+    const onNaver = () => {
+        window.location.href = naver;
+    }
+
     return (
         <>
             <Header text="로그인/회원가입" link="/loginRegister"></Header>
             <div className="logoContainer"></div>
             
             <div className="buttonContainer">
-                <div className="kakao">
+                <div className="kakao" onClick={onKakao}>
                     <span>카카오로 로그인</span>
                 </div>
-                <div className="naver">
+                <div className="naver" onClick={onNaver}>
                     <span>네이버로 로그인</span>
                 </div>
                 
