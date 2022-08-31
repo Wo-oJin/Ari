5be60fbecfe42453be309d61fd7794955e2f453b
YeoInsu <insu12021202@gmail.com>
2022-08-31 15:56:59 +0900

    Add:board page infinite scrolling

--- frontend/src/pages/Board.js
+++ frontend/src/pages/Board.js
@@ -1,18 +1,18 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Header from "../components/Header";
-import { getBoardData } from "../services/board/getBoardData";
 import "./Board.css";
 import { FiSearch } from "react-icons/fi";
 import { IoMdCloseCircle } from "react-icons/io";
 import { Link } from "react-router-dom";
+import { useInView } from "react-intersection-observer";
 
 const BoardItem = ({ boardId, img, title, author, date }) => {
   return (
     <Link to={`/board/${boardId}`}>
       <div className="itemContainer">
         <div className="itemBox">
-          <img src={img}></img>
+          <img className="itemImg" src={"/images/photo.png"}></img>
           <div className="itemContent">
             <span className="itemTitle">{title}</span>
             <span className="itemAuthor">{author}</span>
@@ -24,22 +24,34 @@ const BoardItem = ({ boardId, img, title, author, date }) => {
   );
 };
 const Board = () => {
+  const [ref, inView] = useInView();
+  const [page, setPage] = useState(0);
   const [data, setData] = useState("");
   const [load, setLoad] = useState(true);
   const [search, setSearch] = useState();
+  const getBoardData = async () => {
+    axios.get(`/board/list?page=${page}`).then((response) => {
+      if (response.data) setData((prev) => [...prev, ...response.data]);
+      setLoad(false);
+    });
+  };
+  //page 바뀔 때마다 데이터 불러오기
   useEffect(() => {
-    const getBoardData = async () => {
-      axios.get(`/board/list`).then((response) => {
-        setData(response.data);
-        setLoad(false);
-      });
-    };
+    setLoad(true);
     getBoardData();
-  }, []);
+  }, [page]);
+
+  useEffect(() => {
+    // 사용자가 마지막 요소를 보고 있고, 로딩 중이 아니라면
+    if (inView && !load) {
+      setPage((prevState) => prevState + 1);
+    }
+  }, [inView, load]);
+
   if (load) {
     return <h1>로딩 중</h1>;
   }
-  console.log(data.articleList.content);
+  console.log(data);
 
   //onChange 이벤트 발생할 때마다 검색어 최신화
   const onChangeSearch = (e) => {
@@ -54,10 +66,9 @@ const Board = () => {
       getBoardData();
     } else {
       //기존 데이터 목록에서 검색어가 포함된 데이터만 추출
-      const filterData = data.content.filter((item) =>
-        item.title.includes(search)
-      );
+      const filterData = data.filter((item) => item.title.includes(search));
       setData(filterData);
+      console.log("검색 ", filterData);
     }
     //검색어 초기화
     setSearch("");
@@ -81,7 +92,7 @@ const Board = () => {
           <input
             className="searchInput"
             type="text"
-            value={search}
+            value={search || ""}
             placeholder={"검색어를 입력하세요"}
             onChange={onChangeSearch}
           ></input>
@@ -92,9 +103,10 @@ const Board = () => {
         <button className="writeBtn">작성하기</button>
       </div>
       {data ? (
-        data.articleList.content.map((item, index) => {
+        data.map((item, index) => {
           return (
             <BoardItem
+              key={index}
               boardId={item.id}
               img={""}
               title={item.title}
@@ -106,6 +118,7 @@ const Board = () => {
       ) : (
         <span>data 없음</span>
       )}
+      <div ref={ref}> 무한 스크롤링 옵저버 </div>
     </>
   );
 };
--- frontend/src/services/board/getBoardData.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const POST_BASE_URL = "/board/list";
-
-export const getBoardData = async (setData) => {
-  axios.get(`/board/list`).then((response) => {
-    setData(response.data);
-  });
-};
