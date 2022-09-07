0d1bf581205735c579c76559bbb9005db9f0f6e4
insu12021202 <insu12021202@gmail.com>
2022-09-07 20:21:01 +0900

    Fix:메인페이지 지도의 마커 중복 띄움 현상 수정

--- frontend/src/services/map/KakaoMapScript.js
+++ frontend/src/services/map/KakaoMapScript.js
@@ -10,6 +10,9 @@ const KakaoMapScript = (data, onMarkerClicked) => {
 
   let geoCoder = new kakao.maps.services.Geocoder();
 
+  //여러 개의 마커를 관리하기 위한 배열
+  let markers = [];
+
   for (let i = 0; i < data.length; i++) {
     geoCoder.addressSearch(
       data[i].address.roadAddress,
@@ -18,24 +21,35 @@ const KakaoMapScript = (data, onMarkerClicked) => {
         if (status === kakao.maps.services.Status.OK) {
           var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
 
-          // 결과값으로 받은 위치를 마커로 표시합니다
+          // 결과값으로 받은 위치를 마커로 표시
           var marker = new kakao.maps.Marker({
             map: map,
             position: coords,
             clickable: true,
           });
 
-          // 인포윈도우로 장소에 대한 설명을 표시합니다
+          // 인포윈도우로 장소에 대한 설명을 표시
           var customOverlay = new kakao.maps.CustomOverlay({
             content: `<div style="background-color: #ffffff;border-radius: 20px;border: 2px solid #386ffe; padding: 3px 10px; margin-bottom:40px">${data[i].name}</div>`,
             position: coords,
             yAnchor: 1,
           });
+
+          //마커에 클릭 이벤트 추가
           kakao.maps.event.addListener(marker, "click", function () {
-            // 마커 위에 인포윈도우를 표시합니다
+            //만약 이미 표시된 마커 수가 존재하면 표시된 마커 제거
+            if (markers.length > 0) {
+              let popMarker = markers.pop();
+              console.log(popMarker);
+              popMarker.setMap(null);
+            }
+            // 마커 위에 인포윈도우를 표시
             customOverlay.setMap(map);
+            //방금 표시한 마커를 markers에 저장
+            markers.push(customOverlay);
             map.panTo(coords);
             onMarkerClicked(i);
+            //지도가 클릭되면 마커들이 전부 지워지도록 구현
             kakao.maps.event.addListener(map, "click", () => {
               customOverlay.setMap(null);
             });
