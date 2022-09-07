1fd7856cb397753ec0ebd65dc09fc9839c60a67b
insu12021202 <insu12021202@gmail.com>
2022-09-07 21:50:53 +0900

    Add:게시글 삭제 기능 추가

--- frontend/src/pages/BoardListView.js
+++ frontend/src/pages/BoardListView.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import Header from "../components/Header";
 import "./BoardListView.css";
 import { FcLike, FcLikePlaceholder } from "react-icons/fc";
 const BoardListView = () => {
+  const navigate = useNavigate();
   //좋아요 유무를 확인하기 위한 변수
   const [isFavorited, setIsfavorited] = useState(false);
   //작성자 확인을 위한 변수
@@ -13,6 +14,8 @@ const BoardListView = () => {
   const [data, setData] = useState();
   //게시글 id
   const { articleId } = useParams();
+  //삭제 팝업 확인을 위한 변수
+  const [isDeletePopUpOpend, setIsDeletePopUpOpend] = useState(false);
 
   const getBoardData = async () => {
     axios.get(`/board/list/${articleId}`).then((response) => {
@@ -35,6 +38,25 @@ const BoardListView = () => {
         setIsfavorited(!isFavorited);
         console.log("찜 성공");
       });
+  };
+
+  //수정 버튼 클릭 함수
+  const modifyHandler = () => {};
+
+  //삭제 버튼 클릭시 팝업 토글 함수
+  const togglePopUp = () => {
+    setIsDeletePopUpOpend(!isDeletePopUpOpend);
+  };
+
+  //게시글 삭제 함수
+  const deleteHandler = async () => {
+    await axios.post(`/board/delete/${articleId}`).then((res) => {
+      alert("삭제되었습니다.");
+    });
+    //팝업 닫기
+    setIsDeletePopUpOpend(!isDeletePopUpOpend);
+    //뒤로 이동
+    navigate(-1);
   };
 
   //아직 데이터가 들어오기 전이면 로딩 중 출력
@@ -83,11 +105,28 @@ const BoardListView = () => {
             </div>
             {authority ? (
               <div className="btnBox">
-                <button className="modifyBtn">수정</button>
-                <button className="deleteBtn">삭제</button>
+                <button className="modifyBtn" onClick={modifyHandler}>
+                  수정
+                </button>
+                <button className="deleteBtn" onClick={togglePopUp}>
+                  삭제
+                </button>
               </div>
             ) : null}
           </div>
+          {authority && isDeletePopUpOpend ? (
+            <div className="deletePopUp">
+              <span>삭제하시겠습니까?</span>
+              <div className="popUpBtnBox">
+                <button className="popUpCancleBtn" onClick={togglePopUp}>
+                  취소
+                </button>
+                <button className="popUpDeleteBtn" onClick={deleteHandler}>
+                  삭제
+                </button>
+              </div>
+            </div>
+          ) : null}
         </div>
       </>
     );
