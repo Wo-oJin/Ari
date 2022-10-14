43c2318eaabdd4060c195fe4870df6d56d909f01
salmonco <xkqjsslsek80@naver.com>
2022-10-14 18:39:26 +0900

    Modify: 접근 제한 페이지 처리에 이슈가 있어서 이전 방식으로 되돌림

--- frontend/src/pages/NotFound.js
+++ frontend/src/pages/NotFound.js
@@ -1,13 +1,6 @@
-import React, { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import React from "react";
 
 const NotFound = () => {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    alert("잘못된 접근입니다.");
-    navigate(-1);
-  }, []);
   return <>404 Not Found</>;
 };
 
