27265297d37f0fe27b9f9a8757d27bad6c435cd3
insu12021202 <insu12021202@gmail.com>
2022-11-06 16:37:14 +0900

    Add:notice delete api 구현

--- frontend/src/pages/NoticeArticle.js
+++ frontend/src/pages/NoticeArticle.js
@@ -22,11 +22,16 @@ const NoticeArticle = () => {
   }, []);
 
   const onClick = () => {
-    console.log(articleId);
     navigate(`/notice/modify/${articleId}`);
   };
   const onClickDelete = () => {
     console.log(articleId, "삭제");
+    customAxios.delete(`/admin/notice/${articleId}`).then((res) => {
+      if (res.data.result === "success") {
+        window.alert(res.data.massage);
+        navigate("/notice");
+      }
+    });
   };
   if (!data) {
     return <Loading></Loading>;
